24082db32cd0b07462be5abd2a4f5046cc889d16
rsnelling <robert.snelling@va.gov>
2014-10-28 13:29:45 -0500

    Backend API's Modified for State's Data

--- Dashboard/Prototype/Backend/testApp/expressApi.js
+++ Dashboard/Prototype/Backend/testApp/expressApi.js
@@ -1,10 +1,10 @@
 var sql = require('mssql');
 
 var config = {
-    user: 'XXXXXX',
-    password: 'XXXXXX',
-    server: '000.000.000.000', // You can use 'localhost\\instance' to connect to named instance
-    database: 'Name',
+    user: 'sa',
+    password: 'agile_123',
+    server: '54.225.232.25', // You can use 'localhost\\instance' to connect to named instance
+    database: 'Reach',
 
     options: {
         encrypt: false // Use this if you're on Windows Azure
@@ -13,7 +13,7 @@ var config = {
 
 var express = require('express');
 var app = express();
- 
+
 console.log("Registering endpoint: /");
 app.get('/', function(req, res){
     res.send('hello ROOT world');
@@ -121,5 +121,63 @@ app.get('/attempts', function(req, res){
     
     //res.send(recordset);
 });
+
+app.get('/veteransByState', function(req, res){
+
+    var state = req.param("id");
+    var query = '';
+    if (state) {
+        console.log("Registering endpoint: /veteransByState/:id is " + state);
+        query = "SELECT Branch, count(*) as Total FROM AnalyticsOutput WHERE St = '" + state + "' group by branch";
+        console.log("Query: " + query);
+    } else {
+        query = "SELECT St as State, count(*) as Total FROM AnalyticsOutput group by St";
+    }
+
+    var connection = new sql.Connection(config, function(err) {
+        // ... error checks
+        if (err) { 
+        console.log("Database connection failed!"); 
+        return; 
+        }
+
+        // Query
+        var request = new sql.Request(connection); // or: var request = connection.request();
+        request.query(query, function(err, recordset) {
+            // ... error checks
+            if (err) { 
+            console.log("Query failed!"); 
+            return; 
+            }
+
+            console.log(recordset.length);
+
+            /*for (var i = 0; i < recordset.length; i++) 
+            { 
+                console.log("Row#: " + i + " Last Name: " + recordset[i].lastname + " Firt Name: " + recordset[i].firstname); 
+            } */
+
+            res.send(recordset);
+        });
+
+    });
+    
+    //res.send(recordset);
+});
+
+console.log("Registering endpoint: /jsonendpoint");
+app.get('/jsonendpoint', function(req, res){
+    res.json({
+        "mykey" : "myvalue",
+        "testy" : "something",
+        "exnum" : 123
+    });
+});
+
+console.log("Registering endpoint: /user/:id");
+app.get('/user', function(req, res){
+    res.send('user is ' + req.param("id"));
+}); 
+
  
 app.listen(3000);
