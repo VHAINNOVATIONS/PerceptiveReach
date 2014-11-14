d750b7c85d4abe2f6262b406d2d741b6f456be3a
Paul Bradley <paul.bradley@us.pwc.com>
2014-11-14 15:52:56 -0500

    New SDD, IDS, and Analytic R demo code (Sprint3)

---
+++ Analytics/SourceCode/SprintDemo3_Analytics_RiskModel.R
@@ -0,0 +1,37 @@
+
+require("RODBC")
+
+## Connect to the database
+channel <- odbcConnect("PR", uid="sa", "agile_123")
+data <- sqlQuery(channel, "select * from VeteranRiskAnalytics")
+ind <- runif(20000, min=0, max=1)
+data$ind <- ind
+data$comp <- 0  
+data$comp[data$ind <= 0.05] <- 1
+data$ind <- NULL
+
+keep <- c("UrbanArea",
+          "Age",
+          "Gender",
+          "Race",
+          "HistSubstanceAbuse",
+          "PreviousPsychiatricHospitalization",
+          "PreviousSuicideAttempts",
+          "DiagnosedTBI",
+          "comp") 
+
+modelingData <- data[keep]
+
+model <- glm(formula = comp ~ .,
+             data = modelingData,
+             family = "binomial")
+
+data$Score <- 1000*(model$fitted.values)
+data$comp <- NULL
+
+keep2 <- c('ReachID','Score')
+data2 <- data[keep2]
+
+rm(data)
+
+sqlUpdate(channel, data2, tablename="VeteranRiskAnalytics") 
