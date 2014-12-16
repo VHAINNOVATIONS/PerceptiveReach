ddd991def71bbd02093151fe7e870f99b337609d
Andal FeQuiere <andal.fequiere@us.pwc.com>
2014-12-16 10:09:58 -0500

    fixed changes for CI test

--- Dashboard/PRDash/src/client/app/main/main.controller.spec.js
+++ Dashboard/PRDash/src/client/app/main/main.controller.spec.js
@@ -32,7 +32,7 @@ describe('Controller: MainCtrl', function () {
       .respond([{"State":"AK","Total":1},{"State":"AL","Total":4},{"State":"AR","Total":3},{"State":"AZ","Total":3},{"State":"CA","Total":8},{"State":"ce","Total":1},{"State":"CO","Total":2},{"State":"CT","Total":1},{"State":"DC","Total":1},{"State":"DE","Total":1},{"State":"FL","Total":7},{"State":"GA","Total":3},{"State":"HI","Total":1},{"State":"IA","Total":3},{"State":"ID","Total":1},{"State":"IL","Total":6},{"State":"IN","Total":3},{"State":"KS","Total":2},{"State":"KY","Total":2},{"State":"LA","Total":3},{"State":"LL","Total":2},{"State":"MA","Total":3},{"State":"MD","Total":1},{"State":"ME","Total":1},{"State":"MI","Total":5},{"State":"MN","Total":2},{"State":"MO","Total":5},{"State":"MS","Total":2},{"State":"MT","Total":1},{"State":"NC","Total":4},{"State":"ND","Total":1},{"State":"NE","Total":2},{"State":"NH","Total":1},{"State":"NJ","Total":1},{"State":"NM","Total":1},{"State":"NV","Total":2},{"State":"NY","Total":12},{"State":"OH","Total":5},{"State":"OK","Total":2},{"State":"OR","Total":3},{"State":"PA","Total":8},{"State":"PI","Total":1},{"State":"PR","Total":1},{"State":"RI","Total":1},{"State":"SC","Total":2},{"State":"SD","Total":2},{"State":"TN","Total":4},{"State":"TX","Total":8},{"State":"UT","Total":1},{"State":"VA","Total":3},{"State":"VT","Total":1},{"State":"WA","Total":4},{"State":"WI","Total":3},{"State":"WN","Total":1},{"State":"WV","Total":4},{"State":"WY","Total":2}]);
     */
     $httpBackend.flush();
-    scope.setupMap1(facilitiesCount);
+    scope.setupMap(facilitiesCount);
 
     expect($('#map').html() == '').toBe(false);
     //expect(scope.setupMap(facilitiesCount)).toBe(true);
