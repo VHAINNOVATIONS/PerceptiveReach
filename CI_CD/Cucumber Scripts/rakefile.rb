5ce80928b993187649b6af948a74e32820be5d22
aqspwc <andrew.q.smith@us.pwc.com>
2014-12-17 15:55:50 -0500

    added output to json report
    added output to json report

--- CI_CD/Cucumber Scripts/rakefile.rb
+++ CI_CD/Cucumber Scripts/rakefile.rb
@@ -5,13 +5,13 @@ require 'cucumber/rake/task'
 Cucumber::Rake::Task.new(:features) do |t|
 	t.profile = 'default'
 	#t.cucumber_opts = "features -f html --out=report/report.html"
-	t.cucumber_opts = "features --format json"
+	t.cucumber_opts = "features --format json --out report.json"
 end
 
 Cucumber::Rake::Task.new(:Facility_view_ohio) do |t|
 	t.profile='Facility'
 	#t.cucumber_opts = "features -f html --out=report/report.html"
-	t.cucumber_opts = "features --format json"
+	t.cucumber_opts = "features --format json --out report.json"
 	#t.cucumber_opts = "--tags @Widget"
 end
 task :Facility=> :Facility_view_ohio
