55e342f0cd89cf1872a3a75da86e4b4d8bb7356d
aqspwc <andrew.q.smith@us.pwc.com>
2015-01-07 15:15:11 -0500

    ugh file clean up

--- CI_CD/Cucumber Scripts/rakefile.rb
+++ CI_CD/Cucumber Scripts/rakefile.rb
@@ -7,9 +7,8 @@ Cucumber::Rake::Task.new(:features) do |t|
 end
 
 Cucumber::Rake::Task.new(:Facility_view_ohio) do |t|
-t.profile='Facility'
-#t.cucumber_opts = "--tags @Arizona"
-=======
+	t.profile='Facility'
+	#t.cucumber_opts = "--tags @Arizona"
 	t.profile = 'default'
 	#t.cucumber_opts = "features -f html --out=report/report.html"
 	t.cucumber_opts = "features --format json --out report.json"
