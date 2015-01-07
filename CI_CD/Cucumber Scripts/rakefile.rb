45b459e94f53af855a7126bb2c0949ed44273038
aqspwc <andrew.q.smith@us.pwc.com>
2015-01-07 15:10:46 -0500

    removed syntax error in the rake file

--- CI_CD/Cucumber Scripts/rakefile.rb
+++ CI_CD/Cucumber Scripts/rakefile.rb
@@ -3,7 +3,6 @@ require 'cucumber'
 require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
-<<<<<<< HEAD
   t.profile = 'default'
 end
 
@@ -21,7 +20,6 @@ Cucumber::Rake::Task.new(:Facility_view_ohio) do |t|
 	#t.cucumber_opts = "features -f html --out=report/report.html"
 	t.cucumber_opts = "features --format json --out report.json"
 	#t.cucumber_opts = "--tags @Widget"
->>>>>>> 17f5db02307231da2da35d9bc153dd3462fef330
 end
 task :Facility=> :Facility_view_ohio
 task :default => :features
