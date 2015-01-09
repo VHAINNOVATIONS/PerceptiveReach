5e501f8568e6e84c5e8e75f6107b14d9c6341477
Andal FeQuiere <andal.fequiere@us.pwc.com>
2015-01-09 12:30:51 -0500

    PR-1060 Modularization updates to templates

--- Dashboard/PRDash/src/client/components/sidebar/sidebar.controller.js
+++ Dashboard/PRDash/src/client/components/sidebar/sidebar.controller.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('perceptiveReachApp')
-  .controller('NavigationCtrl', function ($scope, $location, Auth) {
+  .controller('SidebarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
     }];
+    
     $scope.currentUser = Auth.getCurrentUser();
-    console.log($scope.currentUser);
+
     $scope.isCollapsed = true;
 
     $scope.isActive = function(route) {
