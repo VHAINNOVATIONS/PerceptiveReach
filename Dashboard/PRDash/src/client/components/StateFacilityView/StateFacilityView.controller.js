14ba009a2c2c1d839445929e29b0fdc6bb4ddcf4
Andal FeQuiere <andal.fequiere@us.pwc.com>
2015-01-15 05:35:39 -0500

    fixed bug due to backend server changes

--- Dashboard/PRDash/src/client/components/StateFacilityView/StateFacilityView.controller.js
+++ Dashboard/PRDash/src/client/components/StateFacilityView/StateFacilityView.controller.js
@@ -57,7 +57,7 @@ angular.module('perceptiveReachApp')
         };
     
         function getFacilityByState (id){
-            $http.get('/api/facilitiesByState?id=%27'+ id + '%27').success(function(facilitiesByState) {
+            $http.get('/api/facilitiesByState?id='+ id).success(function(facilitiesByState) {
             //console.log(veteransByVAMC);
                 var output = [];
                 var vamc = "";
