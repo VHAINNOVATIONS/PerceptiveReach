ffb579c33675edb9c6f755d68bd31a6591e0dd83
Robert Snelling <robert.snelling@us.pwc.com>
2015-03-04 20:19:16 -0500

    Unit test for Flag and Medication Datamodels and  Medication widget code.

--- Dashboard/PRDash-ADF/server/api/medicationData/medicationData.controller.js
+++ Dashboard/PRDash-ADF/server/api/medicationData/medicationData.controller.js
@@ -14,7 +14,7 @@ var _ = require('lodash');
 // Get list of things
 exports.index = function(req, res) {
   res.header("content-type: application/json");
-
+  console.log("----MedicationDataAPI");
   var data = '[{"ReachID": 1, "Active": 1, "RxID": 1, "Name": "Prescription 1", "Dosage": "100mg"}, {"ReachID": 1, "Active": 0, "RxID": 2, "Name": "Prescription 2","Dosage": "200mg"}, {"ReachID": 1, "Active": 1, "RxID": 3, "Name": "Prescription 3","Dosage": "300mg"}, {"ReachID": 1, "Active": 1, "RxID": 4, "Name": "Prescription 4","Dosage": "400mg"}, {"ReachID": 1, "Active": 0, "RxID": 5, "Name": "Prescription 5","Dosage": "500mg"}]';
 
   res.send(JSON.parse(data));
