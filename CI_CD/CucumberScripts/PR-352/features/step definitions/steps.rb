7961c0be2fb67ad822a037748407945891c18e75
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-06 16:13:02 -0500

    updated some steps

--- CI_CD/CucumberScripts/PR-352/features/step definitions/steps.rb
+++ CI_CD/CucumberScripts/PR-352/features/step definitions/steps.rb
@@ -1,3 +1,3 @@
-Given(/^I am on http:\/\/localhost:(\d+)\/$/) do |arg1|
+Given(/^I navigate to the http:\/\/localhost:(\d+)\/$/) do |arg1|
   visit ('http://localhost:7003') # express the regexp above with the code you wish you had
 end
