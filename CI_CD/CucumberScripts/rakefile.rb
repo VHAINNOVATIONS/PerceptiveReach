40b051f3d7f2be05e6c8379cca7caaed1a42164e
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-06 13:13:30 -0500

    update rake file

--- CI_CD/CucumberScripts/rakefile.rb
+++ CI_CD/CucumberScripts/rakefile.rb
@@ -1,9 +1,11 @@
-#encoding: utf-8 
+﻿#encoding: utf-8 
 require 'cucumber'
 require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
-	t.profile = 'default'
-	t.cucumber_opts = "-r */features --format json --out report.json"
+  t.profile = 'default'
+  t.cucumber_opts = "-r */features --format json --out report.json"
 end
 
+task :default => :features
+#update
