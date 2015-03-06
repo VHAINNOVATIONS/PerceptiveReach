5d0e684c8354339ba6290b73806c7895f971b4b8
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-06 13:31:31 -0500

    update env.rb files

--- CI_CD/CucumberScripts/VeteranContactInfo/features/support/env.rb
+++ CI_CD/CucumberScripts/VeteranContactInfo/features/support/env.rb
@@ -1,14 +1,14 @@
 require 'capybara/cucumber'
 
 Capybara.default_wait_time = 20
-Capybara.default_driver = :selenium_firefox
+Capybara.default_driver = :selenium_chrome
 
 
- Capybara.register_driver :selenium_firefox do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+ Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 Before do |scenario|
 page.driver.browser.manage.window.maximize
 end
-Capybara.javascript_driver = :firefox
+Capybara.javascript_driver = :chrome
