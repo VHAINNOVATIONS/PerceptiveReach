50aafb93d38759c97fa29dbb6857286087f27847
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-09 10:58:16 -0400

    added an expect to the page load

--- CI_CD/CucumberScripts/PR-1151/features/step definitions/steps.rb
+++ CI_CD/CucumberScripts/PR-1151/features/step definitions/steps.rb
@@ -1,3 +1,5 @@
 Given(/^I navigate to the http:\/\/localhost:(\d+)\/$/) do |arg1|
-  visit ('http://localhost:7003') # express the regexp above with the code you wish you had
+  visit ('http://localhost:7003')
+  wait_until do
+  expect(page).to have_content 'Description'
 end
