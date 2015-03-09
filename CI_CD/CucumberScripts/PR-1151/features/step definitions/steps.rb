a800e743a142646ba9c2c094e007cd01c731c78e
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-09 13:01:03 -0400

    fine tune steps

--- CI_CD/CucumberScripts/PR-1151/features/step definitions/steps.rb
+++ CI_CD/CucumberScripts/PR-1151/features/step definitions/steps.rb
@@ -3,7 +3,7 @@ Given(/^I navigate to the http:\/\/localhost:(\d+)\/$/) do |arg1|
   expect(page).to have_content 'Description'
 end
 
-When(/^I click on  "(.*?)"$/) do |view|
+When(/^I Click on  "(.*?)"$/) do |view|
   title = view
   page.find(:xpath, "//span[contains(text(),'#{title}')]").click 
 end
