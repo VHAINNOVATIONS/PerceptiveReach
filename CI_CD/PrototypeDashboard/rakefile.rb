b96f52569b36029a0baa08a4ae15a43fc3bdbbd6
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-09 15:33:04 -0400

    working out some kinks

--- CI_CD/PrototypeDashboard/rakefile.rb
+++ CI_CD/PrototypeDashboard/rakefile.rb
@@ -4,7 +4,8 @@ require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
   t.profile = 'default'
-  t.cucumber_opts = "-r */*/features --format json --out report.json"
+  t.cucumber_opts = "-r */features --format json --out report.json"
 end
 
 task :default => :features
+#update
