df30a81d465d2fb51412e253af1044420fdeee98
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-09 09:53:02 -0400

    trying a mod to the rake file

--- CI_CD/CucumberScripts/rakefile.rb
+++ CI_CD/CucumberScripts/rakefile.rb
@@ -4,7 +4,7 @@ require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
   t.profile = 'default'
-  t.cucumber_opts = "-r */features --format json --out report.json"
+  t.cucumber_opts = "*/features --format json --out report.json"
 end
 
 task :default => :features
