6b454921a51b17343798c8a96cb1fe851273e3a3
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-09 15:35:15 -0400

    updated test rake file

--- CI_CD/PrototypeDashboard/rakefile.rb
+++ CI_CD/PrototypeDashboard/rakefile.rb
@@ -4,7 +4,7 @@ require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
   t.profile = 'default'
-  t.cucumber_opts = "-r */features --format json --out report.json"
+  t.cucumber_opts = "-v -r */ --format json --out report.json"
 end
 
 task :default => :features
