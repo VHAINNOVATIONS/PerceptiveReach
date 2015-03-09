ed8a380808faee63d9fba290183d847a1108265d
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-09 09:39:17 -0400

    update env to include delete all cookies

--- CI_CD/CucumberScripts/SPCWidget/features/support/env.rb
+++ CI_CD/CucumberScripts/SPCWidget/features/support/env.rb
@@ -9,6 +9,7 @@ Capybara.default_driver = :selenium_chrome
 end
 
 Before do |scenario|
+page.driver.browser.manage.delete_all_cookies
 page.driver.browser.manage.window.maximize
 end
 Capybara.javascript_driver = :chrome
