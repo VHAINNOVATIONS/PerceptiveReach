3eb2b31de7b283aec0a32d68f7affac20181a9ce
Andal FeQuiere <andal.fequiere@us.pwc.com>
2015-03-10 23:35:05 -0400

    Added Format Utility Service to standardize display for Date, SSN, and Phone Numbers

--- Dashboard/PRDash-ADF/server/api/veteranRoster/veteranRoster.controller.js
+++ Dashboard/PRDash-ADF/server/api/veteranRoster/veteranRoster.controller.js
@@ -13,6 +13,7 @@ var _ = require('lodash');
 
 var sql = require('mssql');
 
+var dataFormatter = require('../../components/formatUtil/formatUtil.service.js');
 // Get list of things
 exports.index = function(req, res) {
     res.header("content-type: application/json");
@@ -66,8 +67,15 @@ exports.index = function(req, res) {
             }
 
             console.log(recordset.length);
-
-            res.send(recordset);
+            var jsonRecordSet = JSON.parse(JSON.stringify(recordset));
+            //console.log(jsonRecordSet);
+            for (var veteran in jsonRecordSet) {
+                jsonRecordSet[veteran].SSN = dataFormatter.formatData(jsonRecordSet[veteran].SSN);
+                jsonRecordSet[veteran].Phone = dataFormatter.formatData(jsonRecordSet[veteran].Phone);
+                jsonRecordSet[veteran].DateIdentifiedRisk = dataFormatter.formatData(jsonRecordSet[veteran].DateIdentifiedRisk);
+                //console.log(jsonRecordSet[veteran].SSN + " " + jsonRecordSet[veteran].Phone + " " + jsonRecordSet[veteran].DateIdentifiedRisk);
+            }
+            res.send(jsonRecordSet);
         });
 
     });
@@ -87,7 +95,7 @@ exports.update = function(req, res) {
     if (vetReachID) {
         console.log("Registering endpoint: /veteranRoster/:outreachStatus is " + outreachStatus);
         var value = '';
-        if (outreachStatus == null || outreachStatus.length == 0)
+        if (outreachStatus == null || outreachStatus.length == 0 || outreachStatus == 0)
             value = "NULL";
         else
             value = outreachStatus;
