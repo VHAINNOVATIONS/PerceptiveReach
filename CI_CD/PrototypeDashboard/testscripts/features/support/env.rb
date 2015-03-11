b7c12fb4a019a9f18b2373cf9dbbb581aedb07b6
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-11 16:02:05 -0400

    change env back to chrome

--- CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
+++ CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
@@ -1,11 +1,11 @@
 require 'capybara/cucumber'
 
 Capybara.default_wait_time = 15
-Capybara.default_driver = :selenium_firefox
+Capybara.default_driver = :selenium_chrome
 
 
- Capybara.register_driver :selenium_firefox do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+ Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 Before do |scenario|
@@ -14,7 +14,7 @@ Before do |scenario|
 	page.driver.browser.manage.window.resize_to(1920, 1080)
 end
 
-Capybara.javascript_driver = :firefox
+Capybara.javascript_driver = :chrome
 
 Before '@javascript' do
   page.driver.browser.manage.window.resize_to(1920, 1080)
