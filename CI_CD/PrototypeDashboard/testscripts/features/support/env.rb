aa0e57b4876308eba66f1c33119351d17862dc83
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-11 15:59:26 -0400

    trying firefox for screengrab

--- CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
+++ CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
@@ -1,11 +1,11 @@
 require 'capybara/cucumber'
 
 Capybara.default_wait_time = 15
-Capybara.default_driver = :selenium_chrome
+Capybara.default_driver = :selenium_firefox
 
 
- Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+ Capybara.register_driver :selenium_firefox do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 Before do |scenario|
@@ -14,4 +14,8 @@ Before do |scenario|
 	page.driver.browser.manage.window.resize_to(1920, 1080)
 end
 
-Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :firefox
+
+Before '@javascript' do
+  page.driver.browser.manage.window.resize_to(1920, 1080)
+end
