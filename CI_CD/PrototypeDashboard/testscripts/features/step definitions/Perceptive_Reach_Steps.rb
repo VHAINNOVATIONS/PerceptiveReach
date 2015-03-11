102c85d78bdc384cfbe4a79868b918b6dca6cdf5
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-11 15:09:13 -0400

    grabbing some screenshots for troubleshooting

--- CI_CD/PrototypeDashboard/testscripts/features/step definitions/Perceptive_Reach_Steps.rb
+++ CI_CD/PrototypeDashboard/testscripts/features/step definitions/Perceptive_Reach_Steps.rb
@@ -6,11 +6,13 @@ end
 When(/^I click on "(.*?)"$/) do |view|
   title = view
   page.find(:xpath, "//span[contains(text(),'#{title}')]").click #find and click National/State/Facility/Individual View tabe
+  page.save_screenshot('screenshot1.png')
 end
 
 When(/^I Click on "(.*?)"$/) do |view|
   title = view
   page.find(:xpath, "//span[contains(text(),'#{title}')]").click #find and click National/State/Facility/Individual View tabe
+  page.save_screenshot('screenshot2.png')
 end
 
 When(/^I click on close on the "(.*?)" widget$/) do |widgetname|
@@ -50,7 +52,7 @@ And(/^I should see "(.*?)"$/) do |pagecontent|
 end
 
 And(/^I should see "(.*?)" column$/) do |pagecontent|
-  page.save_screenshot('screenshot.png')
+  page.save_screenshot('screenshot3.png')
   expect(page).to have_content(pagecontent)
 end
 
