5df8fa35225e6cf4a8ad76d5b7f2a8ca459216b6
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-11 15:25:12 -0400

    trying selenium window size tricks

--- CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
+++ CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
@@ -10,7 +10,9 @@ end
 
 Before do |scenario|
 	page.driver.browser.manage.delete_all_cookies
-	page.driver.browser.manage.window.maximize
+	#page.driver.browser.manage.window.maximize
+	target_size = Selenium::WebDriver::Dimension.new(1024, 768)
+	page.driver.broswer.manage.window.size = target_size
 end
 
 Capybara.javascript_driver = :chrome
