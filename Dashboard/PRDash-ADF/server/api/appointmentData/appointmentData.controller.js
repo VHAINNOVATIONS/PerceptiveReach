2659a434a573d92831291c84b8dee4ca77529570
Robert Snelling <robert.snelling@us.pwc.com>
2015-03-16 09:23:21 -0400

    PR-1603 - After talking through the new approach for API's that are stubbed out, decided to place the JSON data inline for now.  Update to controller to house data inline rather than pull from external file.

--- Dashboard/PRDash-ADF/server/api/appointmentData/appointmentData.controller.js
+++ Dashboard/PRDash-ADF/server/api/appointmentData/appointmentData.controller.js
@@ -16,7 +16,7 @@ var dataFormatter = require('../../components/formatUtil/formatUtil.service.js')
 // When the Real API get's implemented remove all lines between //removeBegin & //removeEnd
 
 //removeBegin
-var fs = require('fs');
+/*var fs = require('fs');
 var path = require('path');
 var filePath = path.normalize('server/api/appointmentData/');
 var fileName = "appointmentData.json";
@@ -33,7 +33,21 @@ exports.index = function(req, res) {
 	for (var record in jsonRecordSet) {
 	    jsonRecordSet[record].Apptdate = dataFormatter.formatData(jsonRecordSet[record].Apptdate);
 	}
+	res.send(jsonRecordSet);*/
+
+exports.index = function(req, res) {
+  res.header("content-type: application/json");
+  var data = '[{"ReachID": 1,"ApptType": 1,"Apptdate": "2014-02-03T00:00:00.000Z","CancelationType": "0"},{"ReachID": 1,"ApptType": 2,"Apptdate": "2015-01-03T00:00:00.000Z","CancelationType": "1"},{"ReachID": 1,"ApptType": 1,"Apptdate": "2014-12-11T00:00:00.000Z","CancelationType": "0"},{"ReachID": 1,"ApptType": 4,"Apptdate": "2015-03-03T00:00:00.000Z","CancelationType": "2"},{"ReachID": 1,"ApptType": 3,"Apptdate": "2014-11-05T00:00:00.000Z","CancelationType": "0"},{"ReachID": 1,"ApptType": 2,"Apptdate": "2014-04-21T00:00:00.000Z","CancelationType": "1"}]';
+
+  var jsonRecordSet = JSON.parse(data);
+	for (var record in jsonRecordSet) {
+	    jsonRecordSet[record].Apptdate = dataFormatter.formatData(jsonRecordSet[record].Apptdate);
+	}
 	res.send(jsonRecordSet);
+
+
+
+
 //removeEnd
 
 //Begin Real SQL Server API Code
