6a91feb80c44f591f6c7fa279527d59118035005
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-26 10:16:11 -0400

    update env file
    updated env file with support from agress chrome cache removal and sql
    support via tiny_tds

--- CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
+++ CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
@@ -1,22 +1,24 @@
 require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber'
+require "tiny_tds"
 
 Capybara.default_wait_time = 15
 Capybara.default_driver = :selenium_chrome
 
 
  Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-Before do |scenario|
-	page.driver.browser.manage.delete_all_cookies
-	#page.driver.browser.manage.window.maximize
-	page.driver.browser.manage.window.resize_to(1920, 1080)
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --aggressive-cache-discard])
 end
 
 Capybara.javascript_driver = :chrome
 
-Before '@javascript' do
-  page.driver.browser.manage.window.resize_to(1920, 1080)
+Before do |scenario|
+	page.driver.browser.manage.delete_all_cookies
+	page.driver.browser.manage.window.maximize
+	#page.driver.browser.manage.window.resize_to(1920, 1080)
+end
+
+After do |scenario|
+	page.driver.browser.manage.delete_all_cookies
+	page.driver.browser.quit
 end
