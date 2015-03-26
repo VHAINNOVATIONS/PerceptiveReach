56f518a83798d1b72c3bc9b0f8e9a0f7273a8f3e
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-26 10:14:12 -0400

    Revert "updated env support file"
    This reverts commit 02d748edc81721a44a368c62f288fc6d953034be.

--- CI_CD/PrototypeDashboard/control/features/support/env.rb
+++ CI_CD/PrototypeDashboard/control/features/support/env.rb
@@ -5,19 +5,18 @@ Capybara.default_wait_time = 15
 Capybara.default_driver = :selenium_chrome
 
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --aggressive-cache-discard])
+ Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Before do |scenario|
+	page.driver.browser.manage.delete_all_cookies
+	#page.driver.browser.manage.window.maximize
+	page.driver.browser.manage.window.resize_to(1920, 1080)
 end
 
 Capybara.javascript_driver = :chrome
 
-Before do |scenario|
-	page.driver.browser.manage.delete_all_cookies
-	page.driver.browser.manage.window.maximize
-	#page.driver.browser.manage.window.resize_to(1920, 1080)
-end
-
-After do |scenario|
-	page.driver.browser.manage.delete_all_cookies
-	page.driver.browser.quit
+Before '@javascript' do
+  page.driver.browser.manage.window.resize_to(1920, 1080)
 end
