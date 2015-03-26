050ae38ce0898962f8552e06985ffb0ecd666320
aqspwc <andrew.q.smith@us.pwc.com>
2015-03-26 10:14:05 -0400

    updated env support file
    updated env support file to ensure chrome is removing cache after each
    scenario

--- CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
+++ CI_CD/PrototypeDashboard/testscripts/features/support/env.rb
@@ -5,18 +5,19 @@ Capybara.default_wait_time = 15
 Capybara.default_driver = :selenium_chrome
 
 
- Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-Before do |scenario|
-	page.driver.browser.manage.delete_all_cookies
-	#page.driver.browser.manage.window.maximize
-	page.driver.browser.manage.window.resize_to(1920, 1080)
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --aggressive-cache-discard])
 end
 
 Capybara.javascript_driver = :chrome
 
-Before '@javascript' do
-  page.driver.browser.manage.window.resize_to(1920, 1080)
+Before do |scenario|
+	page.driver.browser.manage.delete_all_cookies
+	page.driver.browser.manage.window.maximize
+	#page.driver.browser.manage.window.resize_to(1920, 1080)
+end
+
+After do |scenario|
+	page.driver.browser.manage.delete_all_cookies
+	page.driver.browser.quit
 end
