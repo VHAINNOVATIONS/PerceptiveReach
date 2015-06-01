4542fb66bbda8d77d22d15b08899997d817f4ad1
cgattu <chandrakala.gattu@us.pwc.com>
2015-06-01 14:32:48 -0400

    updated step defini

--- CI_CD/PerceptiveReachTestCases/features/step definitions/Perceptive_Reach_Steps.rb
+++ CI_CD/PerceptiveReachTestCases/features/step definitions/Perceptive_Reach_Steps.rb
@@ -49,6 +49,7 @@ end
 When(/^I leave the page inactive for (\d+) seconds$/) do |arg1|
   #pending # express the regexp above with the code you wish you had
   sleep(900)
+  end
 
 When(/^I click on "(.*?)"$/) do |view|
   title = view
