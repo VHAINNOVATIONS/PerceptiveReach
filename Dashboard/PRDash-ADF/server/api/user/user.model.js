f27bd4c5b5a58a0094c87a20f8b5af0bc3c66ba0
Robert Snelling <robert.snelling@us.pwc.com>
2015-07-09 17:56:07 -0400

    PR-2495 Updates to the User Model to add in the request parameters.

--- Dashboard/PRDash-ADF/server/api/user/user.model.js
+++ Dashboard/PRDash-ADF/server/api/user/user.model.js
@@ -8,21 +8,6 @@ var authTypes = ['github', 'twitter', 'facebook', 'google'];
 var _ = require('lodash');
 var sql = require('mssql');
 
-/*var UserSchema = new Schema({
-  name: String,
-  email: { type: String, lowercase: true },
-  role: {
-    type: String,
-    default: 'user'
-  },
-  hashedPassword: String,
-  provider: String,
-  salt: String,
-  twitter: {},
-  google: {},
-  github: {}
-});*/
-
 var UserSchema = Joi.object().keys({
   UserID: Joi.number().integer().required(),
   UserName: Joi.string().max(50).required(),
@@ -36,21 +21,15 @@ var UserSchema = Joi.object().keys({
 });
 
 var User = function(data){
-  //console.log(data);
   this.salt = this.makeSalt();
   this.data = data; //this.sanitize(data);
-  //console.log(this.data);
   Joi.validate(data, UserSchema, console.log);
-  ////console.log("\n" + data['UserPassword']);
-  //this.hashedPassword = this.encryptPassword(data['UserPassword']);
 }
 
 
 User.prototype.data = {};
 User.prototype.token = {};
 User.prototype.salt = "";
-//User.prototype.hashedPassword = "";
-
 
 User.prototype.getData = function(name){
   return this.data[name];
@@ -65,7 +44,6 @@ User.prototype.getPassword = function(){
 User.prototype.setPassword = function(password){
   this.userPassword = password;
   this.salt = this.makeSalt();
-//  this.hashedPassword = this.encryptPassword(password);
 }
 
 User.prototype.getProfile = function(){
@@ -79,251 +57,131 @@ User.prototype.getToken = function(){
           'role' : this.getData("UserRole")
   };
 }
-/**
- * Virtuals
- */
-/*UserSchema
-  .virtual('password')
-  .set(function(password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashedPassword = this.encryptPassword(password);
-  })
-  .get(function() {
-    return this._password;
-  });
-
-// Public profile information
-UserSchema
-  .virtual('profile')
-  .get(function() {
-    return {
-      'name': this.name,
-      'role': this.role
-    };
-  });
-
-// Non-sensitive info we'll be putting in the token
-UserSchema
-  .virtual('token')
-  .get(function() {
-    return {
-      '_id': this._id,
-      'role': this.role
-    };
-  });*/
-
-/**
- * Validations
- */
-
-/*// Validate empty email
-UserSchema
-  .path('email')
-  .validate(function(email) {
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return email.length;
-  }, 'Email cannot be blank');
-
-// Validate empty password
-UserSchema
-  .path('hashedPassword')
-  .validate(function(hashedPassword) {
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return hashedPassword.length;
-  }, 'Password cannot be blank');
-
-// Validate email is not taken
-UserSchema
-  .path('email')
-  .validate(function(value, respond) {
-    var self = this;
-    this.constructor.findOne({email: value}, function(err, user) {
-      if(err) throw err;
-      if(user) {
-        if(self.id === user.id) return respond(true);
-        return respond(false);
-      }
-      respond(true);
-    });
-}, 'The specified email address is already in use.');*/
 
 User.prototype.validatePresenceOf = function(value) {
   return value && value.length;
 };
 
-/**
- * Pre-save hook
- */
-/*UserSchema
-  .pre('save', function(next) {
-    if (!this.isNew) return next();
 
-    if (!validatePresenceOf(this.hashedPassword) && authTypes.indexOf(this.provider) === -1)
-      next(new Error('Invalid password'));
-    else
-      next();
-  }); */
+User.prototype.makeSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+}
 
-/**
- * Methods
- */
+User.prototype.encryptPassword = function(password) {
+  //console.log("inside encrypt password: " + password);
+  if (!password || !this.salt) return '';
+  var salt = new Buffer(this.salt, 'base64');
+  return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+}
 
- /*User.prototype.authenticate = function(plainText) {
-    //console.log("inside user authentication method");
-    return this.encryptPassword(plainText) === this.hashedPassword;
-  }*/
+User.prototype.sanitize = function(data) {
+  data = data || {};
+  return _.pick(_.defaults(data,UserSchema), _.keys(UserSchema));
+}
 
-  User.prototype.makeSalt = function() {
-    return crypto.randomBytes(16).toString('base64');
-  }
+User.findById = function(id, callback){
+  //console.log("findById: " + id);
+  /*this.findBy({UserID: id},function(err, data) {
+      ////console.log(err);
+      ////console.log("This is data: " + data[0]);
+      if (err) return callback(null, null);
+      if (data == null) return callback(err,null);
+      callback(null, new User(data[0]));
+    });  */
+}
 
-  User.prototype.encryptPassword = function(password) {
-    //console.log("inside encrypt password: " + password);
-    if (!password || !this.salt) return '';
-    var salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  }
-
-  User.prototype.sanitize = function(data) {
-    data = data || {};
-    return _.pick(_.defaults(data,UserSchema), _.keys(UserSchema));
-  }
-
-  User.findById = function(id, callback){
-    //console.log("findById: " + id);
-    /*this.findBy({UserID: id},function(err, data) {
-        ////console.log(err);
-        ////console.log("This is data: " + data[0]);
-        if (err) return callback(null, null);
-        if (data == null) return callback(err,null);
-        callback(null, new User(data[0]));
-      });  */
-  }
-  User.findOne = function(param, callback){
-    //if(param.UserName){
-      this.findBy(param,function(err, data) {
-        ////console.log(err);
-        //console.log("This is data: ", data[0]);
-        if (err) return callback(err, null);
-        if (data == null) return callback(err,null);
-        callback(null, new User(data[0]));
-      });
-      
-    //}
-  }
-  User.findBy = function(key,callback){
-    var data = [];
-
-    var dbc = require('../../config/db_connection/development.js');
-    var appConfig = require('../../config/environment');
-    var config = dbc.config;
-    var sessionStore = appConfig.prSessionStore;
-    var userSessions = sessionStore[key[Object.keys(key)[0]]];
-    var sessioncount = 0;
-    if(userSessions)
-    {
-      sessioncount = Object.keys(userSessions).length;
-    }
-    //var un = req.param("UserName");
-    //var pw = req.param("Password");
-
-    var query = '';
-    if (key) {
-        query = "exec prsystem.sp_GetUser @UserName=" + "'" + key[Object.keys(key)[0]] + "', @ExistingSessionCount=" + sessioncount;
-    } else {
-        ////console.log("ERROR: User Name and Password are required."); 
-        //res.send("ERROR: User Name and Password are required.");
-
-    }
-
-    var connection = new sql.Connection(config, function(err) {
-        // ... error checks
-        if (err || !query) { 
-        data = "Error: Database connection failed!";
-        console.log("Database connection failed!", err); 
-        return; 
-        }
-
-        // Query
-        var request = new sql.Request(connection); // or: var request = connection.request();
-        request.query(query, function(err, recordset) {
-            // ... error checks
-            if (err) { 
-              //console.log("Query failed!", err); 
-              return callback(err.message,null);
-            }
-
-            if (recordset.length < 1){/*console.log("Invalid Key or statement");*/ /*res.send("Invalid key value");*/ return callback("Invalid Key or statement",null);}
-            
-            //console.log(recordset.length);
-            //res.send(recordset);
-            //var record = recordset[0];
-            //console.log("User.findBy - ", JSON.parse(record.DashboardData));//JSON.stringify(JSON.parse(record.DashboardData)));
-            recordset[0].DashboardData = JSON.parse(recordset[0].DashboardData);
-            //console.log("User.findBy - full record", recordset);
-            return callback(null,recordset);
-        });
+User.findOne = function(param, callback){
+  //if(param.UserName){
+    this.findBy(param,function(err, data) {
+      ////console.log(err);
+      if (err) return callback(err, null);
+      if (data == null) return callback(err,null);
+      callback(null, new User(data[0]));
     });
+    
+  //}
+}
+
+User.findBy = function(key,callback){
+  var data = [];
+
+  var dbc = require('../../config/db_connection/development.js');
+  var appConfig = require('../../config/environment');
+  var config = dbc.config;
+  var sessionStore = appConfig.prSessionStore;
+  var userSessions = sessionStore[key[Object.keys(key)[0]]];
+  var sessioncount = 0;
+  if(userSessions)
+  {
+    sessioncount = Object.keys(userSessions).length;
   }
 
-  User.Update = function(key,callback){
-    var dbc = require('../../config/db_connection/development.js');
-    var config = dbc.config;
-    var query = "exec prsystem.sp_UpdateUser @UserName=" + "'" + key[Object.keys(key)[0]] + "', @Status="+"'"+key[Object.keys(key)[1]] +"'";
-    var connection = new sql.Connection(config, function(err) {
+      var connection = new sql.Connection(config, function(err) {
+      // ... error checks
+      if (err) { 
+      data = "Error: Database connection failed!";
+      console.log("Database connection failed!", err); 
+      return; 
+      }
+      var request = new sql.Request(connection); // or: var request = connection.request();
 
-        if (err || !query) { 
-          data = "Error: Database connection failed!";
-          console.log("Database connection failed!", err); 
-          return; 
-        }
+      var userName = key[Object.keys(key)[0]];
+      request.input('userNameParam', sql.VarChar(50), userName);
+      request.input('sessioncount', sql.Int, sessioncount);
 
-        var request = new sql.Request(connection);   
-        request.query(query, function(err, recordset) {
-          if(err)
-          {
+      var query = '';
+      if (key) {
+          query = "exec prsystem.sp_GetUser @UserName=@userNameParam, @ExistingSessionCount=@sessioncount";
+      } else {
+          ////console.log("ERROR: User Name and Password are required."); 
+          //res.send("ERROR: User Name and Password are required.");
+      }        
+
+      // Query
+      request.query(query, function(err, recordset) {
+          // ... error checks
+          if (err) { 
+            //console.log("Query failed!", err); 
             return callback(err.message,null);
           }
+
+          if (recordset.length < 1){
+            return callback("Invalid Key or statement",null);
+          }
+          
+          recordset[0].DashboardData = JSON.parse(recordset[0].DashboardData);
+          //console.log("User.findBy - full record", recordset);
           return callback(null,recordset);
-        });
-    });
-  }
-//UserSchema.methods = {
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-  //authenticate: function(plainText) {
-   // return this.encryptPassword(plainText) === this.hashedPassword;
-  //},
+      });
+  });
+}
 
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
-  //makeSalt: function() {
-   // return crypto.randomBytes(16).toString('base64');
-  //},
+User.Update = function(key,callback){
+  var dbc = require('../../config/db_connection/development.js');
+  var config = dbc.config;
 
-  /**
-   * Encrypt password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
-  //encryptPassword: function(password) {
-  //  if (!password || !this.salt) return '';
-  //  var salt = new Buffer(this.salt, 'base64');
-  //  return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  //}
-//};
+  var connection = new sql.Connection(config, function(err) {
 
-//module.exports = mongoose.model('User', UserSchema);
+      if (err) { 
+        console.dir(err); 
+        return; 
+      }
+      var request = new sql.Request(connection);   
+
+      var userName = key[Object.keys(key)[0]];
+      var status = key[Object.keys(key)[1]];
+      request.input('userNameParam', sql.VarChar(50), userName);
+      request.input('statusParam', sql.VarChar(25), status);
+      var query = "exec prsystem.sp_UpdateUser @UserName=@userNameParam, @Status=@statusParam";
+
+      request.query(query, function(err, recordset) {
+        if(err)
+        {
+          return callback(err.message,null);
+        }
+        return callback(null,recordset);
+      });
+  });
+}
+
 module.exports = User;
