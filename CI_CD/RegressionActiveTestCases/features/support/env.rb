72cb62f83d2bab7acd1b13999c35b8c04c7e4133
aqspwc <andrew.q.smith@us.pwc.com>
2015-08-27 15:06:32 -0400

    update env file - typo
    fixed typo

--- CI_CD/RegressionActiveTestCases/features/support/env.rb
+++ CI_CD/RegressionActiveTestCases/features/support/env.rb
@@ -7,7 +7,7 @@ Capybara.default_wait_time = 15
 Capybara.default_driver = :selenium
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox])
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 
