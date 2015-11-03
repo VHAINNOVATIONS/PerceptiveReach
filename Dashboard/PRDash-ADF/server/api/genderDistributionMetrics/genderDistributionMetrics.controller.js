1bc35ed318735ac9f3abb1250ad53338490864e2
vasudhau <vasudha.upadhyaya@gmail.com>
2015-11-03 10:22:07 -0500

    "PR-3143 # 6h Refactored the AgegroupMetrics to remove the -v and -f  ptions and to use Stat3N and VISN as the 2 request parameters"

--- Dashboard/PRDash-ADF/server/api/genderDistributionMetrics/genderDistributionMetrics.controller.js
+++ Dashboard/PRDash-ADF/server/api/genderDistributionMetrics/genderDistributionMetrics.controller.js
@@ -16,18 +16,19 @@ exports.index = function(req, res) {
             return; 
         }
         var request = new sql.Request(connection);
-        var ID = req.param("ID");
+        
+        /*var ID = req.param("ID");
 
         // Configure WHERE clause if needed
-        var andClause = '';
+        var whereClause = '';
         var trueID = '';
         if(ID != null && ID.indexOf("-v") != -1){
             trueID = ID.split("-v")[0];
-            andClause = " AND v.VISN = @trueID";
+            whereClause = " WHERE v.VISN = @trueID";
         }
         else if(ID != null && ID.indexOf("-f") != -1){
             trueID = ID.split("-f")[0];
-            andClause = " AND s.sta3N = @trueID";
+            whereClause = " WHERE s.sta3N = @trueID";
         }
 
         // Configure Database Query
@@ -35,15 +36,34 @@ exports.index = function(req, res) {
         if (trueID && validator.isInt(trueID)) {
             request.input('trueID', sql.Int, trueID);            
         }
-        query += "SELECT r.RiskLevelDesc as RiskLevel, Gender, COUNT(DISTINCT p.ReachID) as Total"
+        */
+
+         // Configure WHERE clause if needed
+
+          var query = '';
+
+          var sta3N = req.param("sta3N");
+          var VISN = req.param("VISN");
+  
+          var whereClause = ' ';
+  
+          if(VISN && validator.isInt(VISN)){
+              whereClause = " where v.VISN = @VISN ";
+              request.input('VISN', sql.Int,VISN);
+          }    
+          if(sta3N && validator.isInt(sta3N)){
+                  whereClause += " and  s.sta3N = @sta3N ";
+                  request.input('sta3N', sql.Int,sta3N);
+          }
+           
+
+        query += "SELECT r.RiskLevelDesc as RiskLevel, Gender, COUNT(Gender) as Total"
         query += " FROM dbo.Patient p INNER JOIN dbo.Ref_RiskLevel r "
         query += " ON p.RiskLevel = r.RiskLevelID "
         query += " INNER JOIN PatientStation s ON s.ReachID = p.ReachID";
-        query += " INNER JOIN Ref_VAMC v ON s.sta3N = v.STA3N";
-        query += " WHERE Gender IS NOT NULL";
-        query += andClause;
-        query += " GROUP BY r.RiskLevelDesc, p.Gender";
-
+        query += " INNER JOIN Ref_VAMC v ON s.sta3N = v.STA3N ";
+        query += whereClause;
+        query += " AND Gender IS NOT NULL GROUP BY r.RiskLevelDesc, p.Gender";
         
         // Query the database
         request.query(query, function(err, recordset) {
