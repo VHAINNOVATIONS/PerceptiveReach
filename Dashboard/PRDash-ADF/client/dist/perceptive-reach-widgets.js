c74b7c916ea85168945abb9c0eded55dc4205adf
Varun Neelakantan <vneelakant002@nam.ad.pwcinternal.com>
2016-04-20 10:56:20 -0400

    PR-3970 Checking in compiled files related to the previous check in.

--- Dashboard/PRDash-ADF/client/dist/perceptive-reach-widgets.js
+++ Dashboard/PRDash-ADF/client/dist/perceptive-reach-widgets.js
@@ -5782,7 +5782,7 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "\n" +
     "                <div class=\"pull-left\">\r" +
     "\n" +
-    "                  <input type=\"text\" ng-keypress=\"jumpTo($event,'hr')\" class=\"enterDataNumInput\" ng-model=\"hrIndex\"></input>\r" +
+    "                  <input type=\"text\" ng-keypress=\"jumpTo($event,'hr')\" class=\"enterDataNumInput\" ng-model=\"hrIndex\" title=\"High Risk: Jump To Record\"></input>\r" +
     "\n" +
     "                </div>\r" +
     "\n" +
@@ -5810,7 +5810,7 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "\n" +
     "                <div class=\"pull-left\">\r" +
     "\n" +
-    "                  <input type=\"text\" ng-keypress=\"jumpTo($event,'hrspan')\" class=\"enterDataNumInput\" ng-model=\"hrSpanIndex\"></input>\r" +
+    "                  <input title=\"High Risk Span: Jump To Record\" type=\"text\" ng-keypress=\"jumpTo($event,'hrspan')\" class=\"enterDataNumInput\" ng-model=\"hrSpanIndex\"></input>\r" +
     "\n" +
     "                </div>\r" +
     "\n" +
@@ -5860,7 +5860,7 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "\n" +
     "                <div class=\"pull-left\">\r" +
     "\n" +
-    "                  <input class=\"enterDataNumInput\" type=\"text\" ng-keypress=\"jumpTo($event,'mh')\" ng-change=\"mhIndexChange(mhIndex)\" ng-model=\"mhIndex\"></input>\r" +
+    "                  <input title=\"Mental Health Prov.: Jump To Record\" class=\"enterDataNumInput\" type=\"text\" ng-keypress=\"jumpTo($event,'mh')\" ng-change=\"mhIndexChange(mhIndex)\" ng-model=\"mhIndex\"></input>\r" +
     "\n" +
     "                </div>\r" +
     "\n" +
@@ -5914,7 +5914,7 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "\n" +
     "              <div class=\"pull-left\">\r" +
     "\n" +
-    "                <input type=\"text\" ng-keypress=\"jumpTo($event,'sp')\" class=\"enterDataNumInput\" ng-model=\"spIndex\"></input>\r" +
+    "                <input title=\"Safety Plan: Jump To Record\" type=\"text\" ng-keypress=\"jumpTo($event,'sp')\" class=\"enterDataNumInput\" ng-model=\"spIndex\"></input>\r" +
     "\n" +
     "              </div>\r" +
     "\n" +
@@ -5944,7 +5944,7 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "\n" +
     "            <div class=\"pull-left\">\r" +
     "\n" +
-    "              <input type=\"text\" ng-keypress=\"jumpTo($event,'spspan')\" class=\"enterDataNumInput\" ng-model=\"spSpanIndex\"></input>\r" +
+    "              <input title=\"Safety Plan VistA: Jump To Record\" type=\"text\" ng-keypress=\"jumpTo($event,'spspan')\" class=\"enterDataNumInput\" ng-model=\"spSpanIndex\"></input>\r" +
     "\n" +
     "            </div>\r" +
     "\n" +
@@ -5988,7 +5988,7 @@ angular.module("ui.widgets").run(["$templateCache", function($templateCache) {
     "\n" +
     "          <div class=\"pull-left\">\r" +
     "\n" +
-    "            <input type=\"text\" ng-keypress=\"jumpTo($event,'comment')\" class=\"enterDataNumInput\" ng-model=\"commentIndex\"></input>\r" +
+    "            <input title=\"Comments: Jump To Record\" type=\"text\" ng-keypress=\"jumpTo($event,'comment')\" class=\"enterDataNumInput\" ng-model=\"commentIndex\"></input>\r" +
     "\n" +
     "          </div>\r" +
     "\n" +
