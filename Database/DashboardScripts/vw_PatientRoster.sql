cb81dd73fd7893c8799800485c5c7d6199401692
Varun Neelakantan <vneelakant002@nam.ad.pwcinternal.com>
2016-04-29 15:12:39 -0400

    PR-4042 Changes to patient roster view, Joining the new table to filter at risk patients.

--- Database/DashboardScripts/vw_PatientRoster.sql
+++ Database/DashboardScripts/vw_PatientRoster.sql
@@ -1,3 +1,15 @@
+DROP VIEW [dbo].[vw_PatientRoster]
+GO
+
+/****** Object:  View [dbo].[vw_PatientRoster]    Script Date: 4/29/2016 2:59:51 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
 CREATE VIEW [dbo].[vw_PatientRoster] as
 SELECT LastName+','+FirstName as Name, 
 p.ReachID, FirstName, 
@@ -13,5 +25,8 @@ p.RiskScore
 FROM  Patient p 
 INNER JOIN Ref_RiskLevel rl ON rl.RiskLevelID = p.RiskLevel
 INNER JOIN PatientStation ps ON p.ReachID = ps.ReachID 
+INNER JOIN prsystem.PatientDashboardFilter f on p.ReachID = f.ReachIDWHERE f.Patient = 1
 
+
+GO
 
