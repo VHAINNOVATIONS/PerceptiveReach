728d474b84f339bb7499e8201a4fac7705c3067b
Varun Neelakantan <vneelakant002@nam.ad.pwcinternal.com>
2016-04-29 14:58:16 -0400

    PR-4042 Performance improvements for VISN_RosterVw(Bill)

--- Database/DashboardScripts/vw_VISNRoster.sql
+++ Database/DashboardScripts/vw_VISNRoster.sql
@@ -12,41 +12,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-CREATE VIEW [dbo].[vw_VISNRoster] as
-
-WITH cte1 
-AS
--- Define the CTE query.
-(
-  SELECT COUNT( DISTINCT P.ReachID) AS TOTAL, vi.VISN, vi.NetworkName,vi.RegionServed FROM Ref_VISN vi
-	INNER JOIN Ref_VAMC va
-	ON vi.VISN = va.VISN
-	INNER JOIN PatientStation ps 
-	ON va.STA3N = ps.sta3N
-	INNER JOIN Patient p
-	ON ps.ReachID = p.ReachID
-	GROUP BY vi.VISN,vi.NetworkName,vi.RegionServed
-),
-cte2 
-AS
-(
-SELECT COUNT( DISTINCT P.ReachID) AS TOTAL, vi.VISN, vi.NetworkName,vi.RegionServed FROM Ref_VISN vi
-INNER JOIN Ref_VAMC va
-ON vi.VISN = va.VISN
-INNER JOIN PatientStation ps 
-ON va.STA3N = ps.sta3N
-INNER JOIN Patient p
-ON ps.ReachID = p.ReachID
-WHERE p.RiskLevel IS NOT NULL
-GROUP BY vi.VISN,vi.NetworkName,vi.RegionServed
-)
-SELECT t1.total AS TotalPatients,t2.total AS AtRisk, t1.VISN,T1.NetworkName,t1.RegionServed FROM 
-cte1 t1
-INNER JOIN cte2 t2
-ON t1.visn = t2.visn
-
+CREATE VIEW vw_VISNRosterASSELECT c.TotalPat as TotalPatients, c.TotalAtRisk as AtRisk, r.VISN, r.NetworkName, r.RegionServedFROM Ref_VISN r INNER JOIN(SELECT vi.visn, sum(a.TotalPat) as TotalPat, sum(b.TotalAtRisk) as TotalAtRiskFROM Ref_VAMC va INNER JOIN Ref_VISN vi ON va.VISN = vi.VISNINNER JOIN(SELECT count(*) as TotalPat, ps.sta3N  FROM patient p inner join PatientStation ps on p.ReachID = ps.ReachID  GROUP BY ps.sta3N) a on va.STA3N = a.sta3n INNER JOIN(SELECT count(*) as TotalAtRisk, ps1.sta3N  FROM patient p1 inner join PatientStation ps1 on p1.ReachID = ps1.ReachID  WHERE p1.RiskLevel is not null GROUP BY ps1.sta3N) b on va.sta3n = b.sta3nGROUP BY vi.visn) c on r.VISN = c.VISN
 
 GO
 
