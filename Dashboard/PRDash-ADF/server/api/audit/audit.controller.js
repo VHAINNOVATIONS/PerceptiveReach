a74b594eb4bb81841478098045b11ff176ffbe43
Varun Neelakantan <vneelakant002@nam.ad.pwcinternal.com>
2016-07-20 16:21:08 -0400

    PR-4307 Audit log changes for Patient Outreach Status Update.

--- Dashboard/PRDash-ADF/server/api/audit/audit.controller.js
+++ Dashboard/PRDash-ADF/server/api/audit/audit.controller.js
@@ -3,7 +3,7 @@ var praudit = require('../../audit');
 
 exports.index = function(req, res) {
     var action = req.body.action;
-    var message = 'User: ' + req.headers.prsessionkey.split('::')[0];
+    var message = 'User ' + req.headers.prsessionkey.split('::')[0];
     var logType = 'info';
 	praudit.auditlog(action,message,logType);
     res.send('Successfully logged event');
