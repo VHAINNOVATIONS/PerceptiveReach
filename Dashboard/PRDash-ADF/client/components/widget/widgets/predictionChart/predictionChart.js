d52ae362c1384c310f0cddacc9c0057c51199b69
Zach Forrest <zforrest001@nam.ad.pwcinternal.com>
2016-08-22 14:53:23 -0400

    Completed PR-4632 enhance language on attempt prediciton chart widget. Completed PR-4618 updates to CDS widget. Completed PR-4667 updates to attempt prediction chart.

--- Dashboard/PRDash-ADF/client/components/widget/widgets/predictionChart/predictionChart.js
+++ Dashboard/PRDash-ADF/client/components/widget/widgets/predictionChart/predictionChart.js
@@ -33,8 +33,8 @@ return {
       $scope.xAxisTickValuesFunction = function () {
         return function () {
           return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 16, 17];
-        };
-      };
-    }
+         };
+      }; 
+    },
   };
 });
